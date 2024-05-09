12f45418e7f52004534d12c2bb585a15c20b0224
bryan <bryankoh17102@gmail.com>
2024-05-09 18:29:26 +0800

    image SHAP

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 from textblob import TextBlob 
-from transformers import pipeline
+from transformers import pipeline, AutoImageProcessor, AutoModelForImageClassification
 import numpy as np
 from streamlit_shap import st_shap
 from openai import OpenAI
@@ -11,6 +11,7 @@ from PIL import Image, ImageOps
 import cv2
 from io import BytesIO
 from deepface import DeepFace
+import matplotlib.pyplot as plt
 
 load_dotenv()
 shap.initjs()
@@ -112,9 +113,6 @@ if option == "Image Sentiment Analysis":
         image = Image.open(uploaded_file)
 
     st.image(bytes_data)
-    message = pipe(image)
-
-    st.write(message)
 
 elif option == "Video Sentiment Analysis":
     st.title("Video Sentiment Analysis")
@@ -151,87 +149,126 @@ elif option == "Video Sentiment Analysis":
         #st.write(f"Overall sentiment of the video: {overall_sentiment}")
         st.write(message)
 
-
 if st.button("Analyze the Sentiment"): 
-    blob = TextBlob(message) 
-    result = classifier([message])
-    shap_values = explainer([message])
+    if (option == "Text Sentiment Analysis" or option == "Audio Sentiment Analysis"):
+        blob = TextBlob(message) 
+        result = classifier([message])
+        shap_values = explainer([message])
 
-    polarity = result[0]["label"]
-    score = result[0]["score"]
+        polarity = result[0]["label"]
+        score = result[0]["score"]
 
-    st.success(f"The entered text has {polarity} sentiments associated with it."+str(score)) 
-    # st.success(result) 
-    # st.success(shap_values)
+        st.success(f"The entered text has {polarity} sentiments associated with it."+str(score)) 
+        # st.success(result) 
+        # st.success(shap_values)
 
-    st_shap(shap.plots.text(shap_values), height=predict_height(message))
+        st_shap(shap.plots.text(shap_values), height=predict_height(message))
 
-    explanation = shap.Explanation(shap_values)
-    id_to_explain = 0 #id to explain
-    output_to_explain = 1 #positive or negative
+        explanation = shap.Explanation(shap_values)
+        id_to_explain = 0 #id to explain
+        output_to_explain = 1 #positive or negative
 
-    try:
-        st_shap(shap.plots.waterfall(explanation[id_to_explain,:,output_to_explain], max_display=12))
-    except Exception as e:
-        print(f"An error occurred while generating waterfall plot: {e}")
+        try:
+            st_shap(shap.plots.waterfall(explanation[id_to_explain,:,output_to_explain], max_display=12))
+        except Exception as e:
+            print(f"An error occurred while generating waterfall plot: {e}")
 
-    try:
-        st_shap(shap.plots.bar(explanation[id_to_explain,:,output_to_explain]))
-    except Exception as e:
-        print(f"An error occurred while generating bar plot: {e}")
+        try:
+            st_shap(shap.plots.bar(explanation[id_to_explain,:,output_to_explain]))
+        except Exception as e:
+            print(f"An error occurred while generating bar plot: {e}")
 
-    # Given data
-    values = shap_values.values
-    base_values = shap_values.base_values
-    data = shap_values.data
+        # Given data
+        values = shap_values.values
+        base_values = shap_values.base_values
+        data = shap_values.data
 
-    # Create a dictionary to store the values
-    shap_data = {}
+        # Create a dictionary to store the values
+        shap_data = {}
 
-    # Iterate through each entry and store SHAP values along with their corresponding sentences
-    for i, (shap_values, sentence) in enumerate(zip(values, data)):
-        shap_data[i] = {'shap_values': shap_values, 'sentence': sentence}
+        # Iterate through each entry and store SHAP values along with their corresponding sentences
+        for i, (shap_values, sentence) in enumerate(zip(values, data)):
+            shap_data[i] = {'shap_values': shap_values, 'sentence': sentence}
 
-    # Extracting SHAP values and sentences from the dictionary
-    shap_values_0 = shap_data[0]['shap_values']
-    sentence_0 = shap_data[0]['sentence']
+        # Extracting SHAP values and sentences from the dictionary
+        shap_values_0 = shap_data[0]['shap_values']
+        sentence_0 = shap_data[0]['sentence']
 
-    # processed the input
-    zipped_values = list(zip(shap_values_0, sentence_0))
-    sorted_zipped_values = sorted(zipped_values, key=lambda x: x[0][0])
-    if len(sorted_zipped_values) >= 10:
-        selected_values = sorted_zipped_values[:5] + sorted_zipped_values[-5:]
-    else:
-        selected_values = sorted_zipped_values
-
-    modified_list = []
-    for item in selected_values:
-        shap_value_t, word = item
-        if shap_value_t[0] < 0 or shap_value_t[1] > 0:
-            modified_list.append(('positive', round(shap_value_t[1], 3), word))
+        # processed the input
+        zipped_values = list(zip(shap_values_0, sentence_0))
+        sorted_zipped_values = sorted(zipped_values, key=lambda x: x[0][0])
+        if len(sorted_zipped_values) >= 10:
+            selected_values = sorted_zipped_values[:5] + sorted_zipped_values[-5:]
         else:
-            modified_list.append(('negative', round(shap_value_t[0], 3), word))
+            selected_values = sorted_zipped_values
 
-    processed_data = [
-        {"sentiment": entry[0], "score": entry[1], "word": entry[2]} 
-        for entry in modified_list if entry[1] != 0
-    ]
+        modified_list = []
+        for item in selected_values:
+            shap_value_t, word = item
+            if shap_value_t[0] < 0 or shap_value_t[1] > 0:
+                modified_list.append(('positive', round(shap_value_t[1], 3), word))
+            else:
+                modified_list.append(('negative', round(shap_value_t[0], 3), word))
+
+        processed_data = [
+            {"sentiment": entry[0], "score": entry[1], "word": entry[2]} 
+            for entry in modified_list if entry[1] != 0
+        ]
 
 
 
-    client = OpenAI(
-    api_key=os.getenv('OPEN_API_KEY'),
-    )
+        client = OpenAI(
+        api_key=os.getenv('OPEN_API_KEY'),
+        )
 
 
-    completion = client.chat.completions.create(
-    model="gpt-3.5-turbo-0125",
-    messages=[
-        {"role": "system", "content": "I provide insights based on word sentiment scores:"},
-        {"role": "user", "content": f"The overall sentence sentiment is {result}, do backed it up with insightful comments and explaination"},
-        {"role": "user", "content": f"{processed_data}"},
-        {"role": "user", "content": f"Your input sentence is: {message}."},
-    ]
-    )
+        completion = client.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[
+            {"role": "system", "content": "I provide insights based on word sentiment scores:"},
+            {"role": "user", "content": f"The overall sentence sentiment is {result}, do backed it up with insightful comments and explaination"},
+            {"role": "user", "content": f"{processed_data}"},
+            {"role": "user", "content": f"Your input sentence is: {message}."},
+        ]
+        )
 
-    st.write(completion.choices[0].message.content)
+        st.write(completion.choices[0].message.content)
+    else:
+        res = pipe(image)
+        st.write(res)
+
+        image_np = np.array(image)
+        processor = AutoImageProcessor.from_pretrained("trpakov/vit-face-expression")
+        inputs = processor(images=image, return_tensors="pt")
+        model = AutoModelForImageClassification.from_pretrained("trpakov/vit-face-expression")
+
+        def f(img):
+            tmp = img.copy()
+            inputs = processor(images=tmp, return_tensors="pt")
+            outputs = model(**inputs)
+            logits = outputs.logits
+            return logits
+
+        class_names = ["Angry", "Disgusted", "Fearful", "Happy", "Neutral", "Sad", "Surprised"]
+
+        # define a masker that is used to mask out partitions of the input image.
+        masker = shap.maskers.Image("blur(128,128)", image_np.shape)
+
+        # create an explainer with model and image masker
+        explainer = shap.Explainer(f, masker, output_names=class_names)
+
+        # (1, 900, 601, 3)
+        reshaped_img = np.expand_dims(image_np, axis=0)
+
+        # here we explain one images using 500 evaluations of the underlying model to estimate the SHAP values
+        shap_values = explainer(
+            reshaped_img, max_evals=100, batch_size=1, outputs=shap.Explanation.argsort.flip[:4]
+        )
+
+        fig = plt.figure()
+
+        shap.image_plot(shap_values, show=False)
+
+        fig = plt.gcf()
+        fig.set_size_inches(15, 24)
+        st.pyplot(fig)
